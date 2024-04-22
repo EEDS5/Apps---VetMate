8555a2c07de75646d78de70ac232f3208a9b3e5d
EEDS5 <duransandoval777@gmail.com>
2024-04-22 00:34:30 -0400

    Actualización colores de la navegación en DrawerNavigator.js

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -9,28 +9,34 @@ const Drawer = createDrawerNavigator();
 
 // La función getHeaderTitle permite establecer un título dinámico en base a la ruta actual
 const getHeaderTitle = (route) => {
-  // Obtienes el nombre de la ruta activa
-  const routeName = route.state?.routes[route.state.index]?.name ?? route.params?.screen ?? 'Home';
-  
-  // Si la ruta activa es 'Home', devuelve una cadena vacía, de lo contrario devuelve el nombre de la ruta
-  return routeName === 'Home' ? '' : routeName;
+    // Obtienes el nombre de la ruta activa
+    const routeName = route.state?.routes[route.state.index]?.name ?? route.params?.screen ?? 'Home';
+
+    // Si la ruta activa es 'Home', devuelve una cadena vacía, de lo contrario devuelve el nombre de la ruta
+    return routeName === 'Home' ? '' : routeName;
 };
 
 const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen 
-        name="Home" 
-        component={BottomTabNavigator} 
-        options={({ route }) => ({
-          headerTitle: getHeaderTitle(route), // Establece dinámicamente el título del encabezado
-        })}
-      />
-      <Drawer.Screen name="Registro" component={RegistroScreen} />
-      <Drawer.Screen name="Login" component={LoginScreen} />
-      {/* Más Drawer.Screen según sea necesario */}
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerTitle: getHeaderTitle, // Título dinámico para el encabezado
+                drawerActiveTintColor: '#d32f2f', // Color del texto para el elemento activo
+                drawerInactiveTintColor: 'gray', // Color del texto para los elementos inactivos
+                drawerActiveBackgroundColor: '#ffebee', // Un color de fondo suave para el elemento activo que complementa el esquema de colores
+                drawerInactiveBackgroundColor: 'transparent', // Mantenemos los elementos inactivos transparentes
+                itemStyle: { marginVertical: 5 }, // Estilo para cada elemento
+            }}
+        >
+            <Drawer.Screen
+                name="Home"
+                component={BottomTabNavigator}
+            />
+            <Drawer.Screen name="Registro" component={RegistroScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+            {/* Más Drawer.Screen según sea necesario */}
+        </Drawer.Navigator>
+    );
 };
 
 export default DrawerNavigator;
