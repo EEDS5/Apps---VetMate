3dc38e828c07ee6902d25e367d75b45c69d876c0
mikishan65 <miguelangelchavarriacoronado@gmail.com>
2024-05-06 21:07:41 -0400

    corregido fireconfig a vetmet

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,9 +1,8 @@
-//src/firebase/firebaseConfig.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyDIbZ7En7Rui1cneM0viTtSdWh-jeO4HtY",
-  authDomain: "vetmate-498d0.firebaseapp.com",
-  projectId: "vetmate-498d0",
-  storageBucket: "vetmate-498d0.appspot.com",
-  messagingSenderId: "99550247141",
-  appId: "1:99550247141:web:53dc6e06062cd6644abc0c"
+  apiKey: "AIzaSyCMTW4bGLnnW-n5WSzm-KQZIE63TKrhViU",
+  authDomain: "vetmate-73b1d.firebaseapp.com",
+  projectId: "vetmate-73b1d",
+  storageBucket: "vetmate-73b1d.appspot.com",
+  messagingSenderId: "870606359338",
+  appId: "1:870606359338:web:7a9cdaaea64c5d35332c53"
 };
